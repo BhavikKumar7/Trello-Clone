1bf7c8335b3c7519495845f76a1d91c7794242be
BhavikKumar7 <bhavikrajputrr2004@gmail.com>
2026-04-15 21:37:49 +0530

    backend url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://trello-clone-yunl.onrender.com",
+  baseURL: "https://trello-clone-yunl.onrender.com/api",
 });
 
 export default API;
